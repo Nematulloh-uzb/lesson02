d0793ecb879c65afd6a20f4727c4de1d9d179091
Ne'matulloh <nmatullohmansurov@gmail.com>
2026-05-21 09:54:38 +0500

    hgfd

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import img from '../assets/img.jpg'
 
 function PageNotFound() {
   return (
     <div>
-        <h1>PageNotFound</h1>
+        <h1>PageNotFound 404</h1>
+        <img src={img} alt="" width={300} />
     </div>
   )
 }
