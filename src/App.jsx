ec51b8893ea10a49a5348cc58f3292336292adfc
Ne'matulloh <nmatullohmansurov@gmail.com>
2026-05-21 09:32:21 +0500

    dvfcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import About from './components/';
+import About from './components/about';
+import Contact from './components/contact';
+import PageNotFound from './components/PageNotFound';
 
 import './App.css'
 
