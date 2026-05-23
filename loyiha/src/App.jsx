1a6b9bef0d868d7ee45bbd0422af704004444fa4
Ne'matulloh <nmatullohmansurov@gmail.com>
2026-05-23 08:13:48 +0500

    gfd

--- loyiha/src/App.jsx
+++ loyiha/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import About from './components/red.jsx';
-import Contact from './components/blue.jsx';
-import PageNotFound from './components/PageNotFound';
+import About from './components/red.jsx'
+import Contact from './components/blue.jsx'
+import PageNotFound from './components/PageNotFound.jsx'
 import Home from './components/home.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <div className="navbar">
-        <div className="navbar__logo">
-          <h2>jump<span>.</span></h2>
-        </div>
-        <div className="navbar__links">
-          <Link className='Link' to={'/'}>Home</Link>
-          <Link className='Link' to={'/blue'}>Blue</Link>
-          <Link className='Link' to={'/red'}>Red</Link>
+    <BrowserRouter>
+      <div>
+        <div className="navbar">
+          <div className="navbar__logo">
+            <h2>jump<span>.</span></h2>
+          </div>
+
+          <div className="navbar__links">
+            <Link className='Link' to='/'>Home</Link>
+            <Link className='Link' to='/blue'>Blue</Link>
+            <Link className='Link' to='/red'>Red</Link>
+          </div>
         </div>
 
         <Routes>
@@ -28,10 +28,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </div>
-      <div className="navbar__search">
-
-      </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
