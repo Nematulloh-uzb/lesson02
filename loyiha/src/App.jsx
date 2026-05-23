9472925a4d5d69d3655c60b3910d2577c6e0232f
Ne'matulloh <nmatullohmansurov@gmail.com>
2026-05-23 08:05:22 +0500

    gfd

--- loyiha/src/App.jsx
+++ loyiha/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import About from './components/about';
+import Contact from './components/contact';
+import PageNotFound from './components/PageNotFound';
+import Home from './components/home'
 import './App.css'
 
 function App() {
@@ -14,14 +16,38 @@ function App() {
           <h2>jump<span>.</span></h2>
         </div>
         <div className="navbar__links">
-          
+          <Link className='Link' to={'/'}>Home</Link>
+          <Link className='Link' to={'/about'}>About</Link>
+          <Link className='Link' to={'/contact'}>Contact</Link>
         </div>
-        <div className="navbar__search">
 
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <div className="navbar__search">
+
       </div>
     </div>
   )
 }
 
 export default App
+
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className='box'>
+        <h1 className='first'>React-Router-Dom</h1>
+
+      </div>
+
+    </BrowserRouter>
+  )
+}
